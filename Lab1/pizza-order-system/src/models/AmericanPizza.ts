0729297c3839c0a661f5eea5a474c330d689aa7a
Masha003 <colta.masha@gmail.com>
2024-10-27 23:40:59 +0200

    Removed unused classe

--- Lab1/pizza-order-system/src/models/AmericanPizza.ts
+++
@@ -1,8 +0,0 @@
-import { Pizza, PizzaProps } from "./Pizza";
-
-export class AmericanPizza extends Pizza {
-  constructor(props: PizzaProps) {
-    super(props);
-    this.description = `American ${this.description}`;
-  }
-}
--- Lab1/pizza-order-system/src/models/ItalianPizza.ts
+++
@@ -1,8 +0,0 @@
-import { Pizza, PizzaProps } from "./Pizza";
-
-export class ItalianPizza extends Pizza {
-  constructor(props: PizzaProps) {
-    super(props);
-    this.description = `Italian ${this.description}`;
-  }
-}
