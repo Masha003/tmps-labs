d4cbc541437f0366ba2393140e7c5f30dfe3c39f
Masha003 <colta.masha@gmail.com>
2024-11-15 16:44:18 +0200

    Added new toppinig

--- Lab2/pizza-order-system/src/index.ts
+++ Lab2/pizza-order-system/src/index.ts
@@ -1,5 +1,6 @@
 import { ExtraCheese } from "./decorators/ExtraCheese";
 import { Pepperoni } from "./decorators/Pepperoni";
+import { Pineapple } from "./decorators/Pineappe";
 import { PizzaOrderFacade } from "./PizzaOrderFacade";
 
 function main() {
@@ -8,6 +9,7 @@ function main() {
   facade.choosePizzaType("Italian");
   facade.addTopping((pizza) => new ExtraCheese(pizza));
   facade.addTopping((pizza) => new Pepperoni(pizza));
+  facade.addTopping((pizza) => new Pineapple(pizza));
   facade.getPizza();
 
   facade.choosePizzaType("American");
